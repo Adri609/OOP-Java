49d23168ae85f42d41ecc125a285a68cdae4198e
Bogdan Adrián Tucaciuc <adri.bog3@gmail.com>
2025-10-16 10:20:20 +0200

    Library class defined

--- 02_Encapsulation/LibraryEncapsulation/model/Library.java
+++ 02_Encapsulation/LibraryEncapsulation/model/Library.java
@@ -2,5 +2,32 @@ import java.util.ArrayList;
 
 public class Library {
 
-    ArrayList<Book> books = new ArrayList();
+    ArrayList<Book> books = new ArrayList<>();
+    
+    public Library(){
+        books.add(new Book("Los pilares de la tierra", "Ken Follet", 20));
+        books.add(new Book("Narnia", "C. S. Lewis", 30));
+        books.add(new Book("Harry Potter", "J.K. Rowling", 32));
+    }
+
+    public void showAllBooks(){
+
+        for (Book book : books) {
+            System.out.println("Title: " + book.getTitle());
+            System.out.println("Author " + book.getAuthor());
+            System.out.println("Available copies: " + book.getAvailableCopies());
+        }
+    }
+
+    private int countBooks() {
+     
+        int totalBooks = 0;
+        for (Book book : books) {
+            if(book != null){
+                totalBooks++;
+            }
+        }
+        return totalBooks;
+    }
+    
 }
